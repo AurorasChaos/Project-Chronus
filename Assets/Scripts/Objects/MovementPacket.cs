43b8354d5ab3e8f026b9f8b3875a39fc4e298138
AurorasChaos <UnableToAdd@protonmail.ch>
2023-09-13 14:16:40 +0100

    Added new datatype to test lower storage use

--- Assets/Scripts/Objects/MovementPacket.cs
+++ Assets/Scripts/Objects/MovementPacket.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MovementPacket
 {
     public byte playerID;
-    public Vector3 localLocation;
+    public ByteBasedVector3 localLocation;
     public Quaternion localRotation;
     public byte packetCode;
     public byte movementCode;
@@ -13,9 +13,25 @@ public class MovementPacket
     public MovementPacket(byte playerID, Vector3 localLocation, Quaternion localRotation, byte packetCode, byte movementCode)
     {
         this.playerID = playerID;
-        this.localLocation = localLocation;
+        this.localLocation = new ByteBasedVector3(localLocation.x, localLocation.y, localRotation.z);
         this.localRotation = localRotation;
         this.packetCode = packetCode;
         this.movementCode = movementCode;
     }
+}
+
+public class ByteBasedVector3
+{
+    private short X; private short Y; private short Z;
+
+    public ByteBasedVector3(float x, float y, float z) { 
+        this.X = (short)(x * (2^8));
+        this.Y = (short)(y * (2^8));
+        this.Z = (short)(z * (2^8));
+    }
+
+    public float x => this.X / (2 ^ 8);
+    public float y => this.Y / (2 ^ 8);
+    public float z => this.Z / (2 ^ 8);
+
 }
